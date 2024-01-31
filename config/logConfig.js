0498812c3e04b97ec07c7d7b0726d62df3e27538
ds2600 <bill@ishiganto.com>
2024-01-31 06:13:39 -0500

    Fixed missing logs dir

--- config/logConfig.js
+++ config/logConfig.js
@@ -1,5 +1,12 @@
 const winston = require('winston');
 
+const fs = require('fs');
+const dir = './logs';
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
 const syslogFormat = winston.format.printf(({ level, message, timestamp }) => {
     return `${timestamp} ${level.toUpperCase()}: ${message}`;
   });
